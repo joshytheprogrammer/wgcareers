d42710b2e6090d37f670ceaa8304b319a332fc8e
joshytheprogrammer <jurassicjoshua@protonmail.com>
2025-03-29 20:37:11 +0100

    made changes: secured keys

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -16,13 +16,13 @@ export default defineNuxtConfig({
       sessionCookie: true 
     },
     config: {
-      apiKey: "AIzaSyA-8tWb3bycKMTM3hTQTlPYg6nKVnn3Xc0",
-      authDomain: "wg-careers.firebaseapp.com",
-      projectId: "wg-careers",
-      storageBucket: "wg-careers.firebasestorage.app",
-      messagingSenderId: "776646365351",
-      appId: "1:776646365351:web:c5576c91f95b2396bf0b82",
-      measurementId: "G-P360HX9R81"
+      apiKey: process.env.API_KEY,
+      authDomain: process.env.AUTH_DOMAIN,
+      projectId: process.env.PROJECT_ID,
+      storageBucket: process.env.STORAGE_BUCKET,
+      messagingSenderId: process.env.MESSAGE_SENDER_ID,
+      appId: process.env.APP_ID,
+      measurementId: process.env.MEASUREMENT_ID 
     }
   }
 })
